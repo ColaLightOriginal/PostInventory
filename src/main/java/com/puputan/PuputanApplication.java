76a494e3901b633e22296ea3614c110d68194d7d
Patryk Rutkowski <prutkowski@pretius.com>
2020-02-06 19:36:26 +0100

    initial commit

---
+++ src/main/java/com/puputan/PuputanApplication.java
@@ -0,0 +1,13 @@
+package com.puputan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuputanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuputanApplication.class, args);
+	}
+
+}
