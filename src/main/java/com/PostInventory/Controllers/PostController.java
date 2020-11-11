54ff3ab70b444f351bcbe9e014e05bf74ad8d578
Patryk Rutkowski <prutkowski@pretius.com>
2020-11-11 21:22:58 +0100

    change url

--- src/main/java/com/PostInventory/Controllers/PostController.java
+++ src/main/java/com/PostInventory/Controllers/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@RequestMapping(value = "post")
+@RequestMapping(value = "api/posts/post")
 @RestController
 public class PostController {
 
