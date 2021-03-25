3266fb7523fe9fb82593a3c753614c57b0b872dc
Patryk Rutkowski <prutkowski@pretius.com>
2021-03-25 21:57:35 +0100

    Add date central time

--- src/main/java/com/PostInventory/Utlis/DateTimeFormat.java
+++ src/main/java/com/PostInventory/Utlis/DateTimeFormat.java
@@ -1,21 +1,24 @@
 package com.PostInventory.Utlis;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class DateTimeFormat {
 
     static DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
+    static DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
     public static String dateTimeFormat(){
         return formatter.format(LocalDate.now());
     }
 
-    public static ZonedDateTime getZonedDateTime(){
-        Instant nowUtc = Instant.now();
-        ZoneId europeWarsaw = ZoneId.of("Europe/Warsaw");
-        return ZonedDateTime.ofInstant(nowUtc, europeWarsaw);
+    public static Date getZonedDateTime(){
+        OffsetDateTime utc = OffsetDateTime.now(ZoneOffset.UTC);
+//        Instant nowUtc = Instant.now();
+//        ZoneId europeWarsaw = ZoneId.of("Europe/Warsaw");
+//        return ZonedDateTime.ofInstant(nowUtc, europeWarsaw);
+        Date date = Date.from(utc.toInstant());
+        return date;
     }
 }
