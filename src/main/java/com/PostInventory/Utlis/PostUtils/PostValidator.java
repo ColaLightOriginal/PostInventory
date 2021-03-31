3aa2b6b76814b63647bdff65972d4efdf3c830a7
Patryk Rutkowski <prutkowski@pretius.com>
2021-03-31 19:07:56 +0200

    validation fix

--- src/main/java/com/PostInventory/Utlis/PostUtils/PostValidator.java
+++ src/main/java/com/PostInventory/Utlis/PostUtils/PostValidator.java
@@ -24,19 +24,22 @@ public class PostValidator {
 
     private static boolean checkStandardFields(Post post){
         post.log();
-        return post.getTitle() == null || post.getDescription() == null;
+        if(post.getTitle() == null || post.getDescription() == null) return false;
+        return true;
     }
 
     private static boolean checkEventFields(Post post){
         post.log();
-        return post.getTitle() == null || post.getDescription() == null ||
-                post.getPrice() == null || post.getEventDate() == null;
+        if(post.getTitle() == null || post.getDescription() == null ||
+                post.getPrice() == null || post.getEventDate() == null) return false;
+        return true;
     }
 
     private static boolean checkOfferFields(Post post){
         post.log();
-        return post.getTitle() == null || post.getDescription() == null ||
-                post.getPrice() == null;
+        if(post.getTitle() == null || post.getDescription() == null ||
+                post.getPrice() == null) return false;
+        return true;
     }
 
 }
